0d37a165e63361de60608ca40f9d9ffd2081f892
wenhao-lu <wenhaolu.kevin@gmail.com>
2023-03-31 08:27:16 -0400

    update

--- public/my-module.js
+++ public/my-module.js
@@ -1,7 +1,7 @@
 import {theShire, buckland} from './another-module.js'
 
 
-var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+let statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 
 
 export {theShire, statement}
